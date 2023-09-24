5a0e60eb11143a12e64d1784e772ae2b4936a0a4
Sam W <samuel.walton@seven-labs.com>
2023-09-24 15:48:41 -0400

    add static loading

---
+++ loading/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+author 'nano11bravo#2222'
+url 'https://www.impulse101.cc'
+name 'labs-loadingscreen'
+description 'Impulse 101 - Default loading screen for October 2023'
+version '1.0.1'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+loadscreen 'index.html'
